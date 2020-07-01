ba53e480f4b758c3e3c56e20189814aeecd0dfd8
Afarait Sulaimon Bola <afaraitsulaimon@gmail.com>
2020-07-01 07:03:23 -0700

    fix wysiwyg on post message text area

--- users/user-post.php
+++ users/user-post.php
@@ -15,7 +15,7 @@
 <link rel="stylesheet" href="../style.css">
 <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
+    <script src="//cdn.ckeditor.com/4.4.7/full/ckeditor.js"></script>
     
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
@@ -23,7 +23,7 @@
 
 	<title></title>
 </head>
-<body>
+<body onLoad= "iFrameOn();">
 
   <!-- Upper starts from here, it contains menu and directories of each page -->
    
@@ -41,6 +41,13 @@
 						<h1>complaintNG</h1>
 					</a>
                 <!-- Brand ends here-->
+                <div class="ml-auto" style="color:blue; font-size:1.5em; font-weight:bold;">Welcome 
+					<?php 
+					    require_once("user-fullname.php");
+
+
+					  ?>
+					  </div>
 					
 
 					<!-- user dropdown starts from here -->
@@ -48,12 +55,14 @@
 
                          <button class="btn-lg mr-5"><a href="user-post.php">Post</a></button>
                        
-                            <img src="../images/disclaimer.png" class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="width:80px; height:80px;border-radius:50%;">
-                      
+                         <?php
+                                require_once("user-profile-pics.php");
+						  ?>                        
+
                         <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                            <a class="dropdown-item" href="#">Sign Out</a>
-                            <a class="dropdown-item" href="#">Change Password</a>
-                            <a class="dropdown-item" href="#">Edit Profile</a>
+                            <a class="dropdown-item" href="authentication/user-logout-auth.php">Sign Out</a>
+                            <a class="dropdown-item" href="user-changepw.php">Change Password</a>
+                            <a class="dropdown-item" href="user-edit-profile.php">Edit Profile</a>
                         </div>
                     </div>
 
@@ -145,7 +154,7 @@
                <div class="col-3 bg-info">
                     <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
                             <a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">My Posts</a>
-                            <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">New Post</a>
+                            <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false" onLoad= "iFrameOn();">New Post</a>
                             
                     </div>
                    
@@ -178,7 +187,7 @@
                                     echo "<div class='alert alert-danger mt-3'>$messagePostedError</div>";
                                    }
                                ?>
-                               <form method="POST" action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>">
+                               <form method="POST" action="<?php echo htmlentities($_SERVER['PHP_SELF']);?>" id="myForm">
                                    <div class="form-group">
                                        <label>Title:</label>
                                        <input type="text" class="form-control" name='titleForPost'>
@@ -202,16 +211,16 @@
 
                                    </div>
 
-                                   <div class="form-group">
-                                        <label for="exampleFormControlTextarea1">Message</label>
-                                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name='messageForPost'></textarea>
-
+                               
+                                        <textarea style="display:none;" class="form-control" id="exampleFormControlTextarea1 messageForPost" rows="3" name='messageForPost'> 
+                                          
+                                        </textarea>
                                    </div>
 
 
 
                                     <div class="form-group">
-                                        <button type="submit" class="btn-lg" name="postButton">Post</button>
+                                        <button type="submit" class="btn-lg" name="postButton" onClick= "javascript:submit_form();">Post</button>
 
                                     </div>
                                </form>
@@ -257,3 +266,9 @@
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 </html>
+
+
+
+<script>
+   CKEDITOR.replace('messageForPost');
+</script>
